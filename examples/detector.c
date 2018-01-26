7d1638b6321e3fa5fe71f83558e5de0a7598f3a2
tanfulun <swat_tfl@sina.com>
2018-01-26 12:19:08 +0800

    update

--- examples/detector.c
+++ examples/detector.c
@@ -124,7 +124,8 @@ void train_detector(char *datacfg, char *cfgfile, char *weightfile, int *gpus, i
         avg_loss = avg_loss*.9 + loss*.1;
 
         i = get_current_batch(net);
-        printf("%ld: %f, %f avg, %f rate, %lf seconds, %d images\n", get_current_batch(net), loss, avg_loss, get_current_rate(net), what_time_is_it_now()-time, i*imgs);
+        printf("#Batch Number: %ld, BatchLoss: %f, AvgLoss: %f, lr: %f, dur: %lf, %d images\n",
+               get_current_batch(net), loss, avg_loss, get_current_rate(net), what_time_is_it_now()-time, i*imgs);
         if(i%100==0){
 #ifdef GPU
             if(ngpus != 1) sync_nets(nets, ngpus, 0);
